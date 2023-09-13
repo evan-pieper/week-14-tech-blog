dd4a0d6d8aef4e38735fa9aac8d2b8ad393fcaf6
evan-pieper <evanjpieper@gmail.com>
2023-09-12 23:59:46 -0700

    added several dependencies for handling passwords, dealing with databases, and integrating express and handlebars

--- public/js/renderCrossSet.js
+++
@@ -1,5 +0,0 @@
-//const CrossSetConfiguration = document.getElementById('CrossSetConfiguration');
-//for(let i=0; i< CrossSetConfiguration.length; i++)
-//{
-//    console.log(CrossSetConfiguration[i].value);
-//} 
