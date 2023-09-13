f2f9a7e8e3d3ad5a07dfd850b4376830b30618f5
evan-pieper <evanjpieper@gmail.com>
2023-09-12 21:14:12 -0700

    added file structure, and general formating for user creation verification and login

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,82 @@
+const router = require('express').Router();
+const {User} = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', withAuth, async (req, res) => {
+  try {
+    // Get all CrossSet data and JOIN with user data
+    const CrossSetData = await CrossSet.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const CrossSets = CrossSetData.map((CrossSet) => CrossSet.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', { 
+      CrossSets, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/CrossSet/:id', withAuth, async (req, res) => {
+  try {
+    const CrossSetData = await  CrossSet.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const SerialCrossSet = CrossSetData.get({ plain: true });
+
+    res.render('CrossSet', {
+      ...SerialCrossSet,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Use withAuth middleware to prevent access to route
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: CrossSet }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+module.exports = router;
