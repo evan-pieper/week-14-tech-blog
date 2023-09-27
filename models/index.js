71cee47a1e35d18b325102ec1b84eed26dfb5183
evan-pieper <evanjpieper@gmail.com>
2023-09-26 19:38:33 -0700

    added comment.js model for blogpost comments (and updated index accordingly

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Blogpost = require('./Blogpost');
+const Comment = require('./Comment');
 
 
 User.hasMany(Blogpost, {
@@ -11,5 +12,22 @@ Blogpost.belongsTo(User, {
   foreignKey: 'user_id'
 });
 
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'CASCADE'
+});
 
-module.exports = {User, Blogpost};
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+});
+
+Blogpost.hasMany(Comment, {
+  foreignKey: 'blogpost_id',
+  onDelete: 'CASCADE'
+});
+
+Comment.belongsTo(Blogpost, {
+  foreignKey: 'blogpost_id'
+});
+
+module.exports = {User, Blogpost, Comment};
