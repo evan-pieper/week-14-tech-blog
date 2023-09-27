466dae165ce23ca581f132542ad041f62a025e29
evan-pieper <evanjpieper@gmail.com>
2023-09-26 20:48:04 -0700

    added post and delete comment routes

---
+++ controllers/api/commentRoutes.js
@@ -0,0 +1,39 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+router.post('/:id', async (req, res) => {
+    try {
+        const newComment = await Comment.create({
+        ...req.body, //spreads the properties of the req.body object into this new object so that we can add the user_id property to it
+        user_id: req.session.user_id,
+        blogpost_id: req.params.id,
+        });
+    
+        res.status(200).json(newComment);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    try {
+        const commentData = await Comment.destroy({
+            where: {
+                id: req.params.id,
+                user_id: req.session.user_id,
+            },
+        });
+
+        if (!commentData) {
+            res.status(404).json({ message: 'No comment found with this id!' });
+            return;
+        }
+
+        res.status(200).json(commentData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+
+module.exports = router;
