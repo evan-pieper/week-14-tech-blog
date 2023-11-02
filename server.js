eedc4b48b46bf694257c44ad6da1ec0d01c7bd9e
evan-pieper <evanjpieper@gmail.com>
2023-11-01 17:19:30 -0700

    updated dependencies to be heroku friendly

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require('express');
 // Import express-session
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
@@ -17,6 +18,7 @@ const sess = {
   secret: 'Super secret secret',
   resave: false,
   saveUninitialized: true,
+  store: new SequelizeStore({ db: sequelize }),
 };
 //
 app.use(session(sess));
