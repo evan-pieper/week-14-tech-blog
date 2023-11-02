1a11a51e98728aad531ac367fc60b36b0dc0b36f
evan-pieper <evanjpieper@gmail.com>
2023-11-01 17:39:19 -0700

    updated dependencies and added env variable for db password

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 // Set up sessions
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.SECRET,
   cookie: {
     maxAge: 300000,
     httpOnly: true,
